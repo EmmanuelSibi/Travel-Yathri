c5d808325f804dddb959a99f28d4c24f2ad41798
EmmanuelSibi <emmanuelsiby2003@gmail.com>
2023-11-04 13:38:02 +0530

    message from whatsapp is extracted

---
+++ conversation.js
@@ -0,0 +1,9 @@
+//async function named handleusermessage which takes user message and recipient phone as parameters
+async function handleUserMessage(userMessage, recipientPhone) {
+
+
+
+
+}
+
+module.exports = {handleUserMessage}; //exporting handleusermessage function    
--- server.js
+++ server.js
@@ -29,7 +29,36 @@ app.get("/webhook", (req, res) => {
 });
 console.log("hfff");
 
-app.post("/webhook", (req, res) => {
-
-    
-});
+app.post("/webhook", async (req, res) => {
+    try {
+      let data = Whatsapp.parseMessage(req.body);
+  
+      if (data?.isMessage) {
+        let incomingMessage = data.message;
+        let recipientPhone = incomingMessage.from.phone; // extract the phone number of the customer
+        let recipientName = incomingMessage.from.name; // extract the name of the customer
+        let typeOfMsg = incomingMessage.type; // extract the type of message
+        let message_id = incomingMessage.message_id; // extract the message id
+        let userMessage = incomingMessage.text.body;
+        if (currentmsg !== userMessage) {
+          currentmsg = userMessage;
+          console.log(userMessage);
+          const response = await handleUserMessage(userMessage, recipientPhone);
+          console.log(response);   
+  
+          await Whatsapp.sendText({
+            message: `${response}`,
+            recipientPhone: recipientPhone,
+          });
+        } else {
+          console.log("reppp");
+        }
+      }
+  
+      res.status(200).send("Message received and processed");
+    } catch (error) {
+      console.error("Error handling incoming message:", error);
+      
+      res.status(500).send("Internal Server Error");
+    }
+  });
