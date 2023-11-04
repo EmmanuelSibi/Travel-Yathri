4e6e935bc126a4562561c152c107eb26893a35b5
EmmanuelSibi <emmanuelsiby2003@gmail.com>
2023-11-04 12:34:21 +0530

    connecting with whatsapp

--- server.js
+++ server.js
@@ -4,11 +4,32 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 app.use(bodyParser.json());
-  
-const port = process.env.PORT || 3000;  
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-  app.get("/", (req, res) => {
-    res.send("kjehdberdbf");
-  });
+  console.log(`Server is running on port ${port}`);
+});
+app.get("/", (req, res) => {
+  res.send("kjehdberdbf");
+});
+const mytoken = "testing";
+
+app.get("/webhook", (req, res) => {
+  let mode = req.query["hub.mode"];
+  let challange = req.query["hub.challenge"];
+  let token = req.query["hub.verify_token"];
+
+  if (mode && token) {
+    if (mode === "subscribe" && token === mytoken) {
+      res.status(200).send(challange);
+    } else {
+      res.status(403);
+    }
+  }
+});
+console.log("hfff");
+
+app.post("/webhook", (req, res) => {
+
+    
+});
